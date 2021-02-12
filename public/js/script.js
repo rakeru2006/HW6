3893160bd69739066331abdd8976d27a07ad9284
rakeru2006 <rakeru2006@gmail.com>
2021-02-12 17:39:01 -0600

    fixing js funtion

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,11 @@
-
 // Event handler for user clicking the selectSearchCity button
 $("#selectSearchCity").on("click", function(event) {
     // Preventing the button from trying to submit the form
     event.preventDefault();
     // Storing the city name
     var inputcity = $("#weatherInput").val().trim();
-    
-    
-    
-    
-    
     // Running the searchBandsInTown function(passing in the city as an argument)
-    searchWheather(inputcity);
-    
+    searchWheather(inputcity);  
     if (typeof(Storage) !== "undefined") {
     // Store
     localStorage.setItem("city", "inputcity");
@@ -24,13 +17,11 @@ $("#selectSearchCity").on("click", function(event) {
     }
     // Empty the contents of the city-div, append the new city content
       $("#cityDiv").empty();
-    }); 
-        function searchWheather(city) {
-    
+}); 
+
+function searchWheather(city) { 
     // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
-    
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=b96e7e1ded82aa80685e6c10a625dfb9";
-    
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=b96e7e1ded82aa80685e6c10a625dfb9";  
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -44,20 +35,16 @@ $("#selectSearchCity").on("click", function(event) {
         var cityDiv = $("<div class='ciudad'>");
           // Storing the rating data
         
-       
-    
           var cityName = response.city.name;
           console.log(cityName);
           
-    
-    
+
           console.log(cityName);
           // Creating an element to have the rating displayed
           var pOne = $("<p>").text("City: " + cityName);
           // Displaying the name of city
           cityDiv.append(pOne);
           // Storing the temperature
-         
           var dateSearch= response.list[0].dt_txt;
           console.log(dateSearch);
           // Creating an element to hold 
@@ -85,7 +72,6 @@ $("#selectSearchCity").on("click", function(event) {
     
     
          // Storing the release year
-         
         var tempG= response.list[0].wind.speed;
           console.log(tempG);
           // Creating an element to hold the release year
@@ -107,45 +93,31 @@ $("#selectSearchCity").on("click", function(event) {
           
           // Creating an element to hold 
           
-         
+/*
           let ndice = getUv(cityLat,cityLong);
           console.log(ndice);
           var parr5 = $("<p>").text("UV Index: " + ndice);
             cityDiv.append(parr5);
-    
-    
-    
+*/    
             var forecas ="Forecast next 5 day";
-             
-             var titFo = $("<h1>").text(forecas);
+            var titFo = $("<h1>").text(forecas);
              // Displaying the name of city
-             cityDiv.append(titFo);
+            cityDiv.append(titFo);
     
-            for(var k=8; k <41; k++){
-                
-               var dayforecast= response.list[k].dt_txt;
-               var tempf= response.list[k].main.temp;
-               var windGf= response.list[k].wind.speed;
-               var humidf= response.list[k].main.humidity;
-             var parrf= $("<li>").text("Date:" + dayforecast+" "+"Temp:"+tempf+"ºF " +"Wind: "+ windGf+"mph"+" Humid:"+humidf+"%");
-               k= k+8;
-             cityDiv.append(parrf);
+            for(var k=8; k <41; k++){    
+            var dayforecast= response.list[k].dt_txt;
+            var tempf= response.list[k].main.temp;
+            var windGf= response.list[k].wind.speed;
+            var humidf= response.list[k].main.humidity;
+            var parrf= $("<li>").text("Date:" + dayforecast+" "+"Temp:"+tempf+"ºF " +"Wind: "+ windGf+"mph"+" Humid:"+humidf+"%");
+            k= k+8;
+            cityDiv.append(parrf);
             }
-    
-    
-    
     // Storing the humidity
-    
           $("#cityDiv").prepend(cityDiv);
-      
-      //$("#cityDiv").append(cityURL, cityImage, trackerCount, upcomingEvents, goTocity);
-    
-    
-    
-    
-    
+      //$("#cityDiv").append(cityURL, cityImage, trackerCount, upcomingEvents, goTocity);    
     });
-    }
+}
     ///////////Calc UV 
     
     
@@ -154,7 +126,7 @@ $("#selectSearchCity").on("click", function(event) {
     
     
     /////// calc the UV index
-    
+    /*
     function getUv(cityLat,cityLong) {
      var latitud = cityLat;
      var longitud= cityLong;
@@ -174,5 +146,5 @@ $("#selectSearchCity").on("click", function(event) {
         
         return x;      
     }
-    
+    */
     
